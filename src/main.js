44c673c8d6696d67f9b54afb9634a8249a9ba897
deep <deep@bybetter.com>
2019-03-29 13:21:08 -0400

    Web component intialized

--- src/main.js
+++ src/main.js
@@ -1,8 +1,13 @@
 import Vue from 'vue';
+import vueCustomElement from 'vue-custom-element';
 import App from './App.vue';
+import 'document-register-element/build/document-register-element';
 
-Vue.config.productionTip = false;
+Vue.use(vueCustomElement);
+Vue.customElement('vue-custom-element-hello-world-example', App);
 
-new Vue({
-  render: h => h(App),
-}).$mount('#app');
+// Vue.config.productionTip = false;
+//
+// new Vue({
+//   render: h => h(App),
+// }).$mount('#app');
